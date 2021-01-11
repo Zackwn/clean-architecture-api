d3e1e9bab2b02e2838875c111144b7565c2d3442
Zackwn <rafaelfavazzi@gmail.com>
2021-01-11 10:46:00 -0300

    export interface to avoid ts errors

--- src/entities/user/errors/invalid-email.ts
+++ src/entities/user/errors/invalid-email.ts
@@ -1,3 +1,5 @@
+import { DomainError } from "./domain-error"
+
 export class InvalidEmailError extends Error implements DomainError {
   public constructor(email: string) {
     super(`The email "${email}" is invalid.`)
--- src/entities/user/errors/invalid-name.ts
+++ src/entities/user/errors/invalid-name.ts
@@ -1,3 +1,5 @@
+import { DomainError } from "./domain-error"
+
 export class InvalidNameError extends Error implements DomainError {
   public constructor(name: string) {
     super(`The name "${name}" is invalid.`)
