0cd36f1b3f3d859a6d06f3520771d52bd21561bc
Zackwn <rafaelfavazzi@gmail.com>
2021-01-31 17:00:43 -0300

    organize jwt secret

--- src/adapters/auth/jwt-user-auth-adapter.ts
+++ src/adapters/auth/jwt-user-auth-adapter.ts
@@ -6,16 +6,22 @@ import { Either, left, right } from '../../shared/either'
 
 config()
 
+if (!process.env.JWT_SECRET) {
+  throw new Error('Missing JWT_SECRET.')
+}
+
 export class JWTUserAuthAdapter implements UserAuth {
+  private readonly JWT_SECRET: string = String(process.env.JWT_SECRET)
+
   public async sign(payload: UserAuthPayload): Promise<string> {
-    return sign(payload, process.env.JWT_SECRET, {
+    return sign(payload, this.JWT_SECRET, {
       expiresIn: '30m'
     })
   }
 
   public async decode(token: string): Promise<Either<InvalidTokenError, UserAuthPayload>> {
     try {
-      return right(verify(token, process.env.JWT_SECRET) as UserAuthPayload)
+      return right(verify(token, this.JWT_SECRET) as UserAuthPayload)
     } catch (error) {
       return left(new InvalidTokenError())
     }
