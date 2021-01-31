126e3b65b2e628ed9be2f6842ac883b31468b1cd
Zackwn <rafaelfavazzi@gmail.com>
2021-01-31 17:05:02 -0300

    create post route

---
+++ src/main/routes/post-routes.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { ensureAuthAdaptRoute } from "../adapters/with-auth-express-route-adapter";
+import { makeCreatePostController } from "../factories/create-post-controller";
+
+export default (router: Router) => {
+  router.post('/post/create', ensureAuthAdaptRoute(makeCreatePostController()))
+}
