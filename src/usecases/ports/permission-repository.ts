3f790b12487784e0b823dc3701eb78acd5e2589e
Zackwn <rafaelfavazzi@gmail.com>
2021-02-09 18:36:25 -0300

    permission repository add method

--- src/usecases/ports/permission-repository.ts
+++ src/usecases/ports/permission-repository.ts
@@ -2,4 +2,5 @@ import { PermissionData } from "../../entities/permission/permission-data";
 
 export interface PermissionRepository {
   getAllPermissions(): Promise<PermissionData[]>
+  add(permissionData: PermissionData): Promise<PermissionData>
 }
